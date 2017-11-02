060ded956150271f037793a7cb0c5c52324de87a
unknown <905169916@qq.com>
2017-11-02 17:36:48 +0800

    添加日历module

---
+++ app/src/main/java/com/football/freekick/CalenderActivity.java
@@ -0,0 +1,14 @@
+package com.football.freekick;
+
+import android.os.Bundle;
+
+import com.zhy.autolayout.AutoLayoutActivity;
+
+public class CalenderActivity extends AutoLayoutActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calender);
+    }
+}
