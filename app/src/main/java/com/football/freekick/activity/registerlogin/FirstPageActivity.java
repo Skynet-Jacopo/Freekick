5612f29e0e8054f7fefd75d32f0f71b6d6ad303e
Skynet-Jacopo <905169916@qq.com>
2017-11-29 23:36:40 +0800

    hao

--- app/src/main/java/com/football/freekick/activity/registerlogin/FirstPageActivity.java
+++ app/src/main/java/com/football/freekick/activity/registerlogin/FirstPageActivity.java
@@ -2,6 +2,7 @@ package com.football.freekick.activity.registerlogin;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
@@ -10,11 +11,11 @@ import com.football.freekick.App;
 import com.football.freekick.R;
 import com.football.freekick.app.BaseActivity;
 import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.GoogleApiAvailability;
-import com.google.android.gms.common.GooglePlayServicesUtil;
-import com.google.android.gms.tasks.Task;
+
 import com.google.firebase.iid.FirebaseInstanceId;
 import com.orhanobut.logger.Logger;
+
+import java.util.concurrent.atomic.AtomicInteger;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -31,22 +32,42 @@ public class FirstPageActivity extends BaseActivity {
     TextView mTvRegister;
 
     private Context mContext;
+
+    public static final  String EXTRA_MESSAGE                    = "message";
+    public static final  String PROPERTY_REG_ID                  = "registration_id";
+    private static final String PROPERTY_APP_VERSION             = "appVersion";
+    private final static int    PLAY_SERVICES_RESOLUTION_REQUEST = 9000;
+    /**
+     * Substitute you own sender ID here. This is the project number you got
+     * from the API Console, as described in "Getting Started."
+     */
+    String SENDER_ID = "259337742082";
+
+    /**
+     * Tag used on log messages.
+     */
+    static final String TAG = "GCMDemo";
+    AtomicInteger msgId = new AtomicInteger();
+    SharedPreferences prefs;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_firstpage);
         mContext = FirstPageActivity.this;
         ButterKnife.bind(this);
-        Logger.d( "getCountryNameValue--->"+App.mConfig.getCountryNameValue());
-        Logger.d( "getLanguageValue--->"+App.mConfig.getLanguageValue());
-        if (App.mConfig.getLanguageValue().equals("zh")){
+        Logger.d("getCountryNameValue--->" + App.mConfig.getCountryNameValue());
+        Logger.d("getLanguageValue--->" + App.mConfig.getLanguageValue());
+        if (App.mConfig.getLanguageValue().equals("zh")) {
             App.isChinese = true;
-        }else {
+        } else {
             App.isChinese = false;
         }
-        Logger.d("google可用么"+GooglePlayServicesUtil.isGooglePlayServicesAvailable(getApplicationContext()));
+//        Logger.d("google可用么" + GooglePlayServicesUtil.isGooglePlayServicesAvailable(getApplicationContext()));
 //        Logger.d("getToken---->"+ FirebaseInstanceId.getInstance().getToken());
-        int success = ConnectionResult.SUCCESS;
+        int    success = ConnectionResult.SUCCESS;
+        String token   = FirebaseInstanceId.getInstance().getToken();
+        Logger.d("token--->" + token);
     }
 
     @OnClick({R.id.tv_login, R.id.tv_register})
@@ -54,21 +75,22 @@ public class FirstPageActivity extends BaseActivity {
         switch (view.getId()) {
             case R.id.tv_login:
 //                startActivity(new Intent(mContext,LoginPage1Activity.class));
-                startActivity(new Intent(mContext,LoginPager2Activity.class));
+                startActivity(new Intent(mContext, LoginPager2Activity.class));
                 break;
             case R.id.tv_register:
-                startActivity(new Intent(mContext,LoginPage1Activity.class));
+                startActivity(new Intent(mContext, LoginPage1Activity.class));
                 break;
         }
     }
-    private boolean checkPlayServices() {
-        GoogleApiAvailability apiAvailability = GoogleApiAvailability.getInstance();
-//        int resultCode = apiAvailability.isGooglePlayServicesAvailable(app);
-        int resultCode = GooglePlayServicesUtil.isGooglePlayServicesAvailable(getApplicationContext());
-        if (resultCode != ConnectionResult.SUCCESS) {
-            //TODO track user's device not support play service. should use pull to get msg.
-            return false;
-        }
-        return true;
-    }
+//
+//    private boolean checkPlayServices() {
+//        GoogleApiAvailability apiAvailability = GoogleApiAvailability.getInstance();
+////        int resultCode = apiAvailability.isGooglePlayServicesAvailable(app);
+//        int resultCode = GooglePlayServicesUtil.isGooglePlayServicesAvailable(getApplicationContext());
+//        if (resultCode != ConnectionResult.SUCCESS) {
+//            //TODO track user's device not support play service. should use pull to get msg.
+//            return false;
+//        }
+//        return true;
+//    }
 }
